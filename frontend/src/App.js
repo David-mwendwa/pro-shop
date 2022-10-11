5227d36ff01c48fd0130a47f75aa93a44a413285
techdave <klatzdavie19@gmail.com>
2022-10-11 17:56:28 +0300

    create placeOrderScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,8 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 const App = () => {
   return (
@@ -32,8 +34,10 @@ const App = () => {
             <Route path='/register' element={<RegisterScreen />} />
             <Route path='/profile' element={<ProfileScreen />} />
 
-            <Route path='/shipping' element={<ShippingScreen />} />
             {/* <Route path='*' element={<Navigate to='/shipping' replace />} /> */}
+            <Route path='/shipping' element={<ShippingScreen />} />
+            <Route path='/payment' element={<PaymentScreen />} />
+            <Route path='/placeorder' element={<PlaceOrderScreen />} />
           </Routes>
         </Container>
       </main>
