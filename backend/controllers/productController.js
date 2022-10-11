984b31d56446caec528c33592c2f245e0f5bb072
techdave <klatzdavie19@gmail.com>
2022-10-11 18:13:04 +0300

    add order controller and route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,17 +1,21 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-// @desc    Fetch all product
-// @route   GET /api/products
-// @access  Public
+/**
+ * @desc    Fetch all product
+ * @route   GET /api/products
+ * @access  Public
+ */
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.status(200).json(products);
 });
 
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
+/**
+ * @desc    etch single product
+ * @route   GET /api/products/:id
+ * @access  Public
+ */
 const getProductById = asyncHandler(async (req, res) => {
   let id = req.params.id;
   let product = await Product.findById(id);
