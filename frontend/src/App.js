4c86d7c78643d9a04986d0fb1ef246df679e3b0b
techdave <klatzdavie19@gmail.com>
2022-10-12 15:42:01 +0300

    start on admin section - add userlist screen and its logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 
 const App = () => {
   return (
@@ -34,6 +35,7 @@ const App = () => {
             <Route path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />
             <Route path='/profile' element={<ProfileScreen />} />
+            <Route path='/admin/userlist' element={<UserListScreen />} />
 
             {/* <Route path='*' element={<Navigate to='/shipping' replace />} /> */}
             <Route path='/shipping' element={<ShippingScreen />} />
