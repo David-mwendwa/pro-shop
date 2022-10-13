bf8c18150044cc02a4b41b78a398b0ffbdbebba6
techdave <klatzdavie19@gmail.com>
2022-10-13 09:36:27 +0300

    implement frontend image upload

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
@@ -20,6 +21,7 @@ const ProductEditScreen = () => {
   const [category, setCategory] = useState('');
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState('');
+  const [uploading, setUploading] = useState(false);
 
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
@@ -68,12 +70,31 @@ const ProductEditScreen = () => {
     );
   };
 
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('image', file);
+    setUploading(true);
+
+    try {
+      const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+      const { data } = await axios.post('/api/upload', formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
+
+  // TODO: Form.File component is causing the application to crash
+
   return (
     <>
       <Link to='/admin/productlist' className='btn btn-light my-3'>
         Go Back
       </Link>
-
       <FormContainer>
         <h1>Edit Product</h1>
         {loadingUpdate && <Loader />}
@@ -94,7 +115,7 @@ const ProductEditScreen = () => {
             </Form.Group>
 
             <Form.Group controlId='price'>
-              <Form.Label>Price Address</Form.Label>
+              <Form.Label>Price</Form.Label>
               <Form.Control
                 type='number'
                 placeholder='Enter price'
@@ -109,6 +130,13 @@ const ProductEditScreen = () => {
                 placeholder='Enter image url'
                 value={image}
                 onChange={(e) => setImage(e.target.value)}></Form.Control>
+
+              {/* <Form.File
+                id='image-file'
+                label='Choose File'
+                custom
+                onChange={uploadFileHandler}></Form.File>
+              {uploading && <Loader />} */}
             </Form.Group>
 
             <Form.Group controlId='brand'>
